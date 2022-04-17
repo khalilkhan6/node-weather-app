60e2d306b738a84e81b8278d64cd211d3840bc04
unknown <khalilkhan964@gmail.com>
2022-04-17 12:44:12 +0500

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const msgOne = document.getElementById("msgOne");
+const msgTwo = document.getElementById("msgTwo");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  msgOne.textContent = "Loading...";
+  msgTwo.textContent = "";
+  fetch(`http://localhost:3000/weather?address=${location}`).then((response) =>
+    response.json().then((data) => {
+      if (data.error) {
+        msgOne.textContent = data.error;
+      } else {
+        msgOne.textContent = data.location;
+        msgTwo.textContent = data.forcast;
+      }
+    })
+  );
+});
---
+++ src/app.js
@@ -0,0 +1,62 @@
+const exress = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forcast = require("./utils/forcast");
+
+const app = exress();
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "../templates/views"));
+hbs.registerPartials(path.join(__dirname, "../templates/partials"));
+app.use(exress.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+  });
+});
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+  });
+});
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  }
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+      forcast(latitude, longitude, (error, forcastdata) => {
+        if (error) {
+          return res.send({ error });
+        }
+        res.send({
+          forcast: forcastdata,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+});
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    description: "Page not found",
+  });
+});
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
